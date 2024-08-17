ef6db8041f539c1382b1dddae3ed8265970e22f4
bobchen9011 <bob30419@gmail.com>
2024-08-18 02:35:31 +0800

    v1.0.0: Add loading Image 2024-08-18 --- 2:35

--- TaiwanTour/LoadingView.swift
+++ TaiwanTour/LoadingView.swift
@@ -5,7 +5,6 @@
 //  Created by BBOB on 2024/8/18.
 //
 
-import Foundation
 import SwiftUI
 
 struct LoadingView: View {
@@ -13,9 +12,15 @@ struct LoadingView: View {
 
     var body: some View {
         ZStack {
-            Color.white.ignoresSafeArea()
-            GIFView()
-                .frame(width: 10, height: 10) // 設置你的 GIF 顯示大小
+            // 背景圖片
+            Image("LoadingMainPhoto") // 替換為你的圖片名稱
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+
+            // 加載動畫
+            GIFView() // 確保你的 GIFView 正確顯示 GIF
+                .frame(width: 100, height: 100) // 設置你的 GIF 顯示大小
         }
         .onAppear {
             // 模擬加載，3秒後進入主頁面
@@ -23,5 +28,11 @@ struct LoadingView: View {
                 isLoading = false
             }
         }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView(isLoading: .constant(true)) // 使用 .constant(true) 來提供預覽的綁定值
     }
 }
